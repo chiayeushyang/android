1b312783c549000242eaf0a2ed91c546eddf35f7
Chia Yeu Shyang <yeushyang020825@gmail.com>
2022-10-28 02:26:09 +0800

    Quiz App done

--- QuizApp/app/src/main/java/com/firstapp/quizapp/MainActivity.kt
+++ QuizApp/app/src/main/java/com/firstapp/quizapp/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
                     "Please enter your name", Toast.LENGTH_SHORT).show()
             } else {
                 val intent = Intent(this, QuizQuestionActivity::class.java)
+                intent.putExtra(Constants.USER_NAME, et_name.text.toString())
                 startActivity(intent)
                 finish()
             }
